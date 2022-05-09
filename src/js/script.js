5550dd4059952d739cdd5d39936390db404a4b28
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-05-09 19:11:32 +0200

    all size/2

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,8 @@
 const form = document.querySelector("form");
 
-// form.addEventListener("submit", async (event) => {
-//   event.preventDefault();
-
-//   await fetch("/form", {
-//     method: "POST",
-//     body: new FormData(form),
-//   });
-// });
-
 const submitButton = document.querySelector("[type=submit]");
+
+
 
 submitButton.addEventListener("click", async (event) => {
   event.preventDefault();
@@ -18,4 +11,8 @@ submitButton.addEventListener("click", async (event) => {
   const qs = new URLSearchParams(formData).toString();
 
   fetch("/form?" + qs);
+
+
+
+  
 });
