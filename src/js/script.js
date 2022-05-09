7ee8a0133f0d4ff5cce58fff9e34c380c18b313d
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-05-09 20:10:45 +0200

    + js

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,11 @@ const form = document.querySelector("form");
 
 const submitButton = document.querySelector("[type=submit]");
 
+const main = document.querySelector("main");
 
+const footer__logotype = document.querySelector(".footer__logotype");
+
+const content = document.querySelector(".content");
 
 submitButton.addEventListener("click", async (event) => {
   event.preventDefault();
@@ -12,7 +16,17 @@ submitButton.addEventListener("click", async (event) => {
 
   fetch("/form?" + qs);
 
+  main.style.display = "none";
 
+  footer__logotype.style.cssText =
+    "position: absolute; top: 550px; right: 75px";
 
-  
+  const success_field = document.createElement("div");
+
+  success_field.innerHTML = "start for free";
+
+  success_field.style.cssText =
+    "color: #9aa5b5; margin-top: 35px; margin-left: 57px; font-size: 17px; opacity: 0.6; text-transform: uppercase;";
+
+  content.append(success_field);
 });
