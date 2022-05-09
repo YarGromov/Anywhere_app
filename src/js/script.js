8b3734fa4c4f134cfa7d68cfe617693b63e42766
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-05-09 18:46:35 +0200

    all size

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,21 @@
 const form = document.querySelector("form");
 
-form.addEventListener("submit", async (event) => {
+// form.addEventListener("submit", async (event) => {
+//   event.preventDefault();
+
+//   await fetch("/form", {
+//     method: "POST",
+//     body: new FormData(form),
+//   });
+// });
+
+const submitButton = document.querySelector("[type=submit]");
+
+submitButton.addEventListener("click", async (event) => {
   event.preventDefault();
 
-  let response = await fetch("/article/formdata/post/user", {
-    method: "POST",
-    body: new FormData(form),
-  });
+  const formData = new FormData(form);
+  const qs = new URLSearchParams(formData).toString();
+
+  fetch("/form?" + qs);
 });
