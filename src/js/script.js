a75b14c072ba2119c089823c96108657b70a9d41
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-05-17 12:55:14 +0200

    edits

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,14 @@
 const form = document.querySelector("form");
-
 const submitButton = document.querySelector("[type=submit]");
-
 const main = document.querySelector(".main");
-
-const newMain = document.querySelector(".main2");
-
-
 const footer__logotype = document.querySelector(".footer__logotype");
-
 const content = document.querySelector(".content");
-
 const container = document.querySelector(".container");
+const success_field = document.querySelector('.success_field');
+const button_new_form = document.querySelector('.button_new_form');
+
+success_field.style.display = 'none';
+
 
 submitButton.addEventListener("click", async (event) => {
   event.preventDefault();
@@ -23,53 +20,24 @@ submitButton.addEventListener("click", async (event) => {
 
   main.style.display = "none";
 
+  success_field.style.display = '';
+
   footer__logotype.style.cssText =
     "position: absolute; top: 550px; right: 75px";
 
-  const success_field = document.createElement("div");
-
-  const success_field_header = document.createElement("div");
-
-  success_field_header.innerHTML = "start for free";
-
-  success_field_header.classList.add("success_field_header");
-
-  content.append(success_field);
-
-  success_field.append(success_field_header);
-
-  const success_field_result = document.createElement("div");
-
-  success_field_result.innerHTML = "Success!";
-
-  success_field_result.classList.add("success_field_result");
-
-  success_field.append(success_field_result);
-
-  const button_new_form = document.createElement("button");
-
-  button_new_form.classList.add("button_new_form");
-
-  button_new_form.innerHTML = "New Form";
-
-  success_field.append(button_new_form);
 
   button_new_form.addEventListener("click", (e) => {
     e.preventDefault();
 
     success_field.style.display = "none";
 
-    newMain.style.display = "";
+    main.style.display = "block";
 
-    newMain.classList.add('newMain');
-
-    const newForm = document.createElement("div");
-
-    newForm.classList.add("newForm");
-
-    content.append(newForm);
-
-    newForm.append(newMain);
+    document.querySelector('[name="name"]').value = '';
+    document.querySelector('[name="surname"]').value = '';
+    document.querySelector('[name="email"]').value = '';
+    document.querySelector('[name="password"]').value = '';
+    // document.querySelector('[type="radio"]').value = '';
 
   });
 });
