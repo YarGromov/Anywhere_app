294cad18e1b3430e450e249c0dd76af0be38c5e3
YarGromov <1l1l1arr1l1l1@gmail.com>
2022-05-24 17:21:59 +0200

    radio, spread

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,15 @@
 const form = document.querySelector("form");
-const submitButton = document.querySelector("[type=submit]");
 const main = document.querySelector(".main");
 const footerLogotype = document.querySelector(".footer__logotype");
-const content = document.querySelector(".content");
-const container = document.querySelector(".container");
 const successField = document.querySelector(".success_field");
 const buttonNewForm = document.querySelector(".button_new_form");
-const radioButton = document.querySelector('input[name="question"]:checked');
+const inputs = document.querySelectorAll('input');
 
 successField.style.display = "none";
 
-form.addEventListener("submit", (e) => e.preventDefault());
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
 
-submitButton.addEventListener("click", async () => {
   const formData = new FormData(form);
   const qs = new URLSearchParams(formData).toString();
   fetch("/form?" + qs);
@@ -29,9 +26,7 @@ buttonNewForm.addEventListener("click", () => {
 
   footerLogotype.classList.remove("footer__logotype_success");
 
-  document.querySelector('[name="name"]').value = "";
-  document.querySelector('[name="surname"]').value = "";
-  document.querySelector('[name="email"]').value = "";
-  document.querySelector('[name="password"]').value = "";
+  [...inputs].map(input => input.value = '');
+
   document.querySelector('input[name="question"]:checked').checked = false;
 });
